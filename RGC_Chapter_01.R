b7a891982ad8f748d64ec966886a3dd5962564b5
than9th <kunkai.su@gmail.com>
2017-08-26 21:19:30 +0800

    Chapter_01 modified.

---
+++ RGC_Chapter_01.R
@@ -0,0 +1,12 @@
+install.packages("gglot2", "gcookbook")
+library(ggplot2)
+
+install.packages("gcookbook")
+library(gcookbook)
+
+data <- read.csv("datafile.csv")
+
+# 手动为列名赋值
+names(data) <- c("Column1", "Column2", "Column3")
+
+data <- read.csv("datafile.csv", sep = "\t")
